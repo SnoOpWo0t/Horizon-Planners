3a0bf892c6f1ab064d5203445f157cd8c2b1f7ec
SnoOpWo0t <afrinsultanaakhi138@gmail.com>
2026-06-30 04:47:09 +0600

    vercel

--- event_manager/horizon_planner/settings.py
+++ event_manager/horizon_planner/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATICFILES_DIRS = []
+if (BASE_DIR / 'static').exists():
+    STATICFILES_DIRS.append(BASE_DIR / 'static')
 
 # Media files
 MEDIA_URL = 'media/'
